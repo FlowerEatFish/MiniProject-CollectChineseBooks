53b3e3844e9109b79b3e2bf3d5ef77b113df7be4
FlowerEatingFish <flower.baron.519106+github@gmail.com>
2017-04-20 13:25:53 +0800

    Create a link beteen main.py and read.py.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # coding=UTF-8
 from simpleparser import *
+from read import *
 from bookcollection import *
 import time
 
-isbn = [9789862892077, 9789579556873 , 9789863360902, 9789888357802]
+isbn = IsbnCollection()
 
-for i in isbn:
-    parser = SimpleParser(i)
-    data = BookCollection(parser.data)
-    print(data.result)
-    time.sleep(10)
+for i in isbn.isbnList:
+    if type(i) is int:
+        parser = SimpleParser(i)
+        data = BookCollection(parser.data)
+        print(data.result)
+        time.sleep(10)
