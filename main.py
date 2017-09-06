7c9002326e322e15bc77f9c8a5a4623f08757803
FlowerEatFish <flower.baron.519106+github@gmail.com>
2017-09-06 17:30:22 +0800

    Fix a bug

--- main.py
+++ main.py
@@ -4,13 +4,24 @@ from read import *
 from book import *
 import write
 import time
+import re
+
+def isIntType(value):
+    try:
+        int(value)
+        return True
+    except:
+        return False
 
 isbn = IsbnCollection()
 database = []
 
 for i in isbn.isbnList:
-    if type(i) is int:
-        parser = SimpleParser(i)
+    # regular expression. remove non-number.
+    targetIsbn = re.sub(r'\D', "", i)
+    if isIntType(targetIsbn):
+        targetIsbn = int(targetIsbn)
+        parser = SimpleParser(targetIsbn)
         if parser.data == '':
             continue
         else:
